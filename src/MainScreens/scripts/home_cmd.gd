ac8de16b851e6c48fe2a0cb3149827d75c37462d
Elvis Chen <elvischen2002@gmail.com>
2024-06-20 12:56:47 +0800

    Transition_1 Done, fix bugs
    had a folders bugs that i fixed, then i made transition_1, needs to put on Project->Project Settings -> Autoload, transition scene.

--- src/MainScreens/scripts/home_cmd.gd
+++ src/MainScreens/scripts/home_cmd.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/test-scenes/test-button.tscn")
+	Trans1.change_scene("")
 	pass # Replace with function body.
 
 
