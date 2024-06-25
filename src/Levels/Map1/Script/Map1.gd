e93aee18d5193f8569ea06c8a3a7dfcc5d264a04
Luciano <lucianozhuo123@gmail.com>
2024-06-25 08:40:31 +0800

    Little Witch
    Ready for commit

--- src/Levels/Map1/Script/Map1.gd
+++ src/Levels/Map1/Script/Map1.gd
@@ -6,7 +6,7 @@ const CAM_START_POS := Vector2i(600, 320)
 var score: float
 
 var speed : float
-const START_SPEED : float = 8.0
+const START_SPEED : float = 2.0
 const MAX_SPEED : float = 25
 var screen_size : Vector2i
 
@@ -18,8 +18,8 @@ func _ready():
 func new_game():
 	#reset all nodes
 	score = 0
-	$Ape.position = APE_START_POS
-	$Ape.velocity = Vector2i(0,0)
+	$Little_Witch.position = APE_START_POS
+	$Little_Witch.velocity = Vector2i(0,0)
 	$Camera2D.position = CAM_START_POS
 	$Scroll.position = Vector2i(0, 0)
 
@@ -27,7 +27,7 @@ func _process(_delta):
 	speed = START_SPEED 
 	
 	#move Ape and camera
-	$Ape/Body.position.x += speed 
+	$Little_Witch/Body.position.x += speed 
 	$Camera2D.position.x += speed
 	
 	#update ground position
