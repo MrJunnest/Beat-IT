4c39617896359585dca9fc1434854289145995b6
MemerZxZ <135417441+MemerZxZ@users.noreply.github.com>
2024-06-29 08:40:59 +0800

    title
    a

--- src/MainScreens/scripts/home_cmd.gd
+++ src/MainScreens/scripts/home_cmd.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready():
+	$Title.play("default")
+
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://src/Levels/Level_Selector/selector_screens.tscn")
 	#get_tree().change_scene_to_file("")
