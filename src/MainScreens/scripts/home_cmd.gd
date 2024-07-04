de9c2f40be48fe5321280d585ac52ca82aeea7e6
MemerZxZ <135417441+MemerZxZ@users.noreply.github.com>
2024-07-04 10:39:38 +0800

    settings
    settings

--- src/MainScreens/scripts/home_cmd.gd
+++ src/MainScreens/scripts/home_cmd.gd
@@ -1,8 +1,18 @@
+class_name Main_menu
 extends Control
 
-func _ready():
-	$Title.play("default")
+#initialize 
+@onready var VBOX = $"VBox-Main" as VBoxContainer
+@onready var Title = $"VBox-Main/Title" as AnimatedSprite2D
+@onready var Play_button = $"VBox-Main/Play-Button" as Button
+@onready var Settings_button = $"VBox-Main/Settings-Button" as Button
+@onready var Quit_button = $"VBox-Main/Quit-Button" as Button
+@onready var setting_menu = $Settings_menu
 
+func _ready():
+	Title.play("default")
+	handle_signal()
+	
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://src/Levels/Level_Selector/selector_screens.tscn")
 	#get_tree().change_scene_to_file("")
@@ -14,11 +24,24 @@ func _on_play_button_mouse_entered():
 func _on_settings_button_mouse_entered():
 	$Hitsound.play()
 
-
-
 func _on_quit_button_mouse_entered():
 	$Hitsound.play()
 	
+	
+func _on_settings_button_pressed(): # setting button visibility
+	setting_menu.set_process(true)
+	VBOX.visible = false
+	Title.visible = false
+	setting_menu.visible = true
+	
+func _on_exit_settings() : #exit setting button pressed, revert to main menu
+	VBOX.visible = true
+	setting_menu.visible = false
+	Title.visible = true 
+
 func _on_quit_button_pressed():
 	get_tree().quit()
 	pass # Replace with function body.
+
+func handle_signal(): #a place for lost signals...(it's a meme mf, just a place to tidy up the signals)
+	setting_menu.exit_settings.connect(_on_exit_settings) #exit button from settings scene
---
+++ src/MainScreens/settings/settings.gd
@@ -0,0 +1,27 @@
+class_name settings
+extends Control
+
+@onready var button = $MarginContainer/VBoxContainer/Back_but as Button
+@onready var setting_menu = $"." 
+
+signal exit_settings
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	button.button_down.connect(on_exit_pressed)
+	set_process(false)
+	
+func on_exit_pressed() -> void:
+	#print("EXIT DAMMIT") #debug line incase some twat code line decide it doesn't want to work
+	emit_signal("exit_settings")
+	set_process(false)
+
+func _on_volume_value_changed(value): #volume slider, change value change volume
+	AudioServer.set_bus_volume_db(0, value)
+
+func _on_resolution_b_item_selected(index):
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920,1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1280,720))
