5186afd5237894c21b6858e3d009e74bd74db514
Elvis Chen <elvischen2002@gmail.com>
2024-07-04 16:30:52 +0800

    intro audio 三老

--- src/Extra/Intro/Intro.gd
+++ src/Extra/Intro/Intro.gd
@@ -5,6 +5,8 @@ extends CanvasLayer
 func _ready():
 	$AnimationPlayer.play("fade out")
 	await get_tree().create_timer(2).timeout
+	$AudioStreamPlayer.play()
+	await get_tree().create_timer(1).timeout
 	$AnimationPlayer.play("fade in")
 	await get_tree().create_timer(3).timeout
 	get_tree().change_scene_to_file("res://src/Extra/MainScreen/Main.tscn")
